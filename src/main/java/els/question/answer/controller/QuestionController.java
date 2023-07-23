37df3e6eb14eb64186f89ada3432217d7365c68a
ericmaniraguha <ericmaniraguha@gmail.com>
2023-07-23 15:50:47 +0200

    refactor exception and error handling

--- src/main/java/els/question/answer/controller/QuestionController.java
+++ src/main/java/els/question/answer/controller/QuestionController.java
@@ -1,6 +1,7 @@
 package els.question.answer.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,53 +13,85 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class QuestionController {
-   
-	@Autowired
-	private QuestionService questionService;
+
+    @Autowired
+    private QuestionService questionService;
 
     @GetMapping("/allQuestions")
     public ResponseEntity<List<Question>> getAllQuestions() {
-        List<Question> questions = questionService.getAllQuestions();
-        return ResponseEntity.ok(questions);
+        try {
+            List<Question> questions = questionService.getAllQuestions();
+            if (!questions.isEmpty()) {
+                return ResponseEntity.ok(questions);
+            } else {
+                return ResponseEntity.noContent().build();
+            }
+        } catch (Exception ex) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
     }
-    
-    @GetMapping("/questionsByCategory/{category}") // Removed unnecessary curly braces from the path
+
+    @GetMapping("/questionsByCategory/{category}")
     public ResponseEntity<List<Question>> getQuestionsByCategory(@PathVariable String category) {
-        List<Question> questions = questionService.getQuestionsByCategory(category);
-        return ResponseEntity.ok(questions);
+        try {
+            List<Question> questions = questionService.getQuestionsByCategory(category);
+            if (!questions.isEmpty()) {
+                return ResponseEntity.ok(questions);
+            } else {
+                return ResponseEntity.noContent().build();
+            }
+        } catch (Exception ex) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
     }
-    
-//    //method for adding single question
-//    @PostMapping("/addQuestion")
-//    public ResponseEntity<Question> addQuestion(@RequestBody Question question) {
-//        Question addedQuestion = questionService.addQuestion(question);
-//        return ResponseEntity.ok(addedQuestion);
-//    }
-    
-    // method for adding multiple questions
+
     @PostMapping("/addQuestions")
     public ResponseEntity<List<Question>> addQuestions(@RequestBody List<Question> questions) {
-        List<Question> addedQuestions = questionService.addQuestions(questions);
-        return ResponseEntity.ok(addedQuestions);
+        try {
+            List<Question> addedQuestions = questionService.addQuestions(questions);
+            if (!addedQuestions.isEmpty()) {
+                return ResponseEntity.status(HttpStatus.CREATED).body(addedQuestions);
+            } else {
+                return ResponseEntity.badRequest().body(null);
+            }
+        } catch (IllegalArgumentException ex) {
+            return ResponseEntity.badRequest().body(null);
+        } catch (Exception ex) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
     }
-    
+
     @PutMapping("/updateQuestion/{id}")
     public ResponseEntity<Question> updateQuestion(@PathVariable Long id, @RequestBody Question updatedQuestion) {
-        Question question = questionService.updateQuestion(id, updatedQuestion);
-        if (question != null) {
-            return ResponseEntity.ok(question);
-        } else {
-            return ResponseEntity.notFound().build();
+        try {
+            Question question = questionService.updateQuestion(id, updatedQuestion);
+            if (question != null) {
+                return ResponseEntity.ok(question);
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+        } catch (IllegalArgumentException ex) {
+            return ResponseEntity.badRequest().body(null);
+        } catch (Exception ex) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
         }
     }
-    
+
+
     @DeleteMapping("/deleteQuestion/{id}")
     public ResponseEntity<String> deleteQuestion(@PathVariable Long id) {
-        boolean isDeleted = questionService.deleteQuestion(id);
-        if (isDeleted) {
-            return ResponseEntity.ok("Question with ID " + id + " deleted successfully.");
-        } else {
-            return ResponseEntity.notFound().build();
+        try {
+            boolean isDeleted = questionService.deleteQuestion(id);
+            if (isDeleted) {
+                return ResponseEntity.ok("Question with ID " + id + " deleted successfully.");
+            } else {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Question with ID " + id + " not found.");
+            }
+        } catch (IllegalArgumentException ex) {
+            return ResponseEntity.badRequest().body("Invalid question ID.");
+        } catch (Exception ex) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred while deleting the question.");
         }
     }
+
 }
