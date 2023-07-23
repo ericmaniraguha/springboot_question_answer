6b1649a3dab0b05095823df98916b5cdf402050e
ericmaniraguha <ericmaniraguha@gmail.com>
2023-07-23 14:29:16 +0200

    I am done with all features of crud operations

--- src/main/java/els/question/answer/dao/QuestionRepository.java
+++ src/main/java/els/question/answer/dao/QuestionRepository.java
@@ -1,5 +1,7 @@
 package els.question.answer.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import els.question.model.Question;
 
 @Repository
 public interface QuestionRepository extends JpaRepository<Question, Long> {
-
+    List<Question> findByCategory(String category);
 }
